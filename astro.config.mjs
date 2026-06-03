f6c1b0b71e3772cf95cb9759bbf85d3b0077396e
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2026-06-03 19:17:45 +0200

    scaffolded Astro SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
+import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
   },
