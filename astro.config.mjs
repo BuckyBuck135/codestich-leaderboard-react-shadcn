fa75ead9f042f1edbe8827d9a8be6494c49c4546
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2026-06-19 14:36:09 +0200

    social, og and seo meta

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://codestitch-community-leaderboard.netlify.app",
   output: "server",
   adapter: netlify(),
   vite: {
